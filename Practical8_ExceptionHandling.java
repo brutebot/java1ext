491d9b4d13949aca5579e0744787f17cc950f30a
Vedant Kande <154390051+brutebot@users.noreply.github.com>
2026-06-09 21:02:22 +0530

    Practical8_ExceptionHandling.java

--- Practical8_ExceptionHandling.java
+++ Practical8_ExceptionHandling.java
@@ -8,7 +8,7 @@ class ExceptionDemo {
             int result = numerator / denominator;
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
-            System.out.println("Error: Cannot divide by zero!");
+            System.out.println("Error: Cannot divide by zero..!!");
         } finally {
             System.out.println("This block always executes.");
         }
